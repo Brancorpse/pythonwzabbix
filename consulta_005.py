e1f376550d981405ff12d3db8c641eaf681653f9
brancorpse-admin <lim.machina@gmail.com>
2022-05-30 23:47:01 -0300

    Adding new file

--- consulta_005.py
+++ consulta_005.py
@@ -4,42 +4,18 @@ import apizabbix
 # English: Data correlation using Python. We'll catch data from all events of a group to make a filter
 
 api = apizabbix.connect()
-hostgroups = api.hostgroup.get(
-    output=['id'],
-    filter={
-        'name': 'Portal'
-    },
-)
+
 # Adicionado filtro
 # English: Adding filter
 
-events = api.event.get(
-    output=[
-        'clock',
-        'name',
-        'value',
-        'severity',
-    ],
-    groupids=hostgroups[0]['groupid'],
-)
-# Lista de severidades
-# English: severity list
-severidades = [
-    'Não classificada',
-    'Informação',
-    'Atenção',
-    'Alta',
-    'Desastre'
-]
+historico = api.history.get({
+    'itemids': [23668],
+    'history':0,
+    'output': 'extend',
+    'time_from': 1438387200,
+    'time_till': 1439250959
+})
+print(historico)
 
-from datetime import datetime
-for event in events:
-    hora_evento = datetime.fromtimestamp(
-        int(event['clock'])
-    ) .strftime('%Y-%m-%d %H: %M:%S')
-    severidade = severidades[(int(event['severity']))]
-    print(
-        'Evento: '+event['name']+ ',  ocorrido em: '+ hora_evento +' com severidade ' + severidade
-    )
 
 api.user.logout()
